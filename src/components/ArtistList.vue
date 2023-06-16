2cf38783ae662a1a07353c5a87bc0275fa2c4098
Aline <aline.rose@mail.uft.edu.br>
2023-06-16 11:06:38 -0300

    :recycle: Refatoracao na paginacao

--- src/components/ArtistList.vue
+++ src/components/ArtistList.vue
@@ -1,50 +1,49 @@
 <template>
     <div id="Artist">
-        <button
+        <!-- <button
             class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">
             <router-link :to="'/createArtist/'">Criar um artista</router-link>
         </button>
+ -->
+        <div class="flex h-screen gap-8 justify-center items-center flex-wrap">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 container">
+            
 
-
-        <div class="flex flex-cols gap-6 justify-center items-center flex-wrap">
-            <div section="w-full" v-for="artist in artists" :key="artist.id">
-                <img class=" w-20 rounded-xl" :src="artist.imageURL" alt="Artist cover" />
+            <div class="grid justify-items-center " v-for="artist in artists" :key="artist.id">
+                <div class="justify-center items-center">
+        
+                <img class="w-40 h-40 rounded-full mb-4" :src="artist.imageURL" alt="Artist cover" />
+                    
+                <button
+                    class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110">
+                    <router-link :to="'/artist/' + artist.id">{{ artist.name }}</router-link>
+                </button>
+                      
             </div>
-
+              
+        </div>  
         </div>
-
-        <!-- <div class="min-h-screen bg-gray-100 mx-auto flex items-center">
-            <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300"
-                v-for="artist in artists" :key="artist.id">
-                <img class="rounded-xl" :src="artist.imageURL" alt="Artist cover" />
-                <div class="flex justify-between items-center">
-                    <div>
-                        <h1 class="mt-5 text-2xl font-semibold">{{ artist.name }}</h1>
-                    </div>
-                    <div>
-                        <button
-                            class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110"
-                            @click="deleteArtist(artist.id)">
-                            Delete
-                        </button>
-                        <button
-                            class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110">
-                            <router-link :to="'/artist/' + artist.id">{{ artist.name }}</router-link>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
+        </div>
+        <button
+            v-if=" totalPages"
+            @click="fetchNextPage"
+            class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110"
+        >
+            Próxima página
+        </button>
     </div>
 </template>
   
 <script>
 import axios from 'axios';
+
 export default {
     name: 'ArtistList',
     data() {
         return {
             artists: [],
+            currentPage: 1,
+            totalPages: 1,
         };
     },
     mounted() {
@@ -54,15 +53,29 @@ export default {
         fetchArtists() {
             axios
                 .get('http://127.0.0.1:8000/api/artist/')
-          
                 .then(response => {
-                    this.artists = response.data.results;
-                    console.log('this', this.artists);
+
+                this.totalPages = response.data.count;
+                this.artists = response.data.results;
+                console.log(this.totalPages)
                 })
                 .catch(error => {
-                    console.log(error);
+                console.log(error);
                 });
-        },
+    },
+    fetchNextPage() {
+        const nextPage = this.currentPage + 1;
+        axios
+            .get(`http://127.0.0.1:8000/api/artist/?page=${nextPage}`)
+            .then(response => {
+            const newArtists = response.data.results;
+            this.artists.push(...newArtists);
+            this.currentPage = nextPage;
+            })
+            .catch(error => {
+            console.log(error);
+            });
+    },
         deleteArtist(artistId) {
             axios
                 .delete(`http://127.0.0.1:8000/api/artist/${artistId}/`)
@@ -74,6 +87,7 @@ export default {
                     console.log(error);
                 });
         },
+
     },
 };
 </script>
